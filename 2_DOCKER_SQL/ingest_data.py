1d2ffc25bea83dc88a8505d24ad272609db4d3a2
carlosaquinoperez <39394611+carlosaquinoperez@users.noreply.github.com>
2024-01-23 11:17:58 +0000

    updated

--- 2_DOCKER_SQL/ingest_data.py
+++ 2_DOCKER_SQL/ingest_data.py
@@ -18,13 +18,12 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    parquet_name = 'output.parquet'
+    parquet_name = 'yellow_tripdata_2021-01.parquet'
 
-    os.system(f"wget {url} -o {parquet_name}")
+    os.system(f"wget {url} -O {parquet_name}")
 
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-    engine.connect()
 
     chunk_size = 100000
     parquet_file = pq.ParquetFile(parquet_name)
@@ -49,13 +48,13 @@ if __name__== '__main__':
 
     #user , password, host, port , database name, table name,
     #url of the parquet
-    parser.add_argument('user', help='user name for postgres')
-    parser.add_argument('pass', help='pass for postgres')
-    parser.add_argument('host', help='host for postgres')
-    parser.add_argument('port', help='port for postgres')
-    parser.add_argument('db', help='database for postgres')
-    parser.add_argument('table-name', help='name of the table where we will write the results to')
-    parser.add_argument('url', help='url of the parquet file')
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
 
 
 args = parser.parse_args()
